3d442deca0ed003ecdf93bdd73bd6cff37a423f4
Aviana <winblocker@gmx.de>
2019-06-06 23:20:54 +0200

    Upgrade to classic

--- HealComm.lua
+++ HealComm.lua
@@ -1,33 +1,201 @@
-local HealComm = AceLibrary("HealComm-1.0")
-local AceEvent = AceLibrary("AceEvent-2.0")
-local roster = AceLibrary("RosterLib-2.0")
+local libCHC = LibStub("LibClassicHealComm-1.0", true)
 
 OVERHEALPERCENT = 20
+
+HealComm = select(2, ...)
+HealComm.version = 2000
+
 local frames = {
-				["player"] = getglobal("PlayerFrameHealthBar"),
-				["target"] = getglobal("TargetFrameHealthBar"),
-				["party1"] = getglobal("PartyMemberFrame1HealthBar"),
-				["party2"] = getglobal("PartyMemberFrame2HealthBar"),
-				["party3"] = getglobal("PartyMemberFrame3HealthBar"),
-				["party4"] = getglobal("PartyMemberFrame4HealthBar"),
+				["player"] = { bar = getglobal("PlayerFrameHealthBar"), frame = _G["PlayerFrame"] },
+				["pet"] = { bar = getglobal("PetFrameHealthBar"), frame = _G["PetFrame"] },
+				["target"] = { bar = getglobal("TargetFrameHealthBar"), frame = _G["TargetFrame"] },
+				["party1"] = { bar = getglobal("PartyMemberFrame1HealthBar"), frame = _G["PartyMemberFrame1"] },
+				["party2"] = { bar = getglobal("PartyMemberFrame2HealthBar"), frame = _G["PartyMemberFrame2"] },
+				["party3"] = { bar = getglobal("PartyMemberFrame3HealthBar"), frame = _G["PartyMemberFrame3"] },
+				["party4"] = { bar = getglobal("PartyMemberFrame4HealthBar"), frame = _G["PartyMemberFrame4"] },
 				}
 
-				
-				
-				
-local function onHeal(unit, frameID)
+local partyGUIDs = {
+	[UnitGUID("player")] = "player",
+}
+local raidGUIDs = {}
+local currentHeals = {}
+
+local function RaidPullout_UpdateHook(pullOutFrame)
 	local frame
-	if not frameID then
-		frame = frames[unit]
-	else
-		frame = frameID
+	for i=1, pullOutFrame.numPulloutButtons do
+		frame = getglobal(pullOutFrame:GetName().."Button"..i.."HealthBar")
+		if not frame.incheal then
+			frame.incHeal = CreateFrame("StatusBar", pullOutFrame:GetName().."Button"..i.."HealthBarIncHeal" , frame)
+			frame.incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+			frame.incHeal:SetMinMaxValues(0, 1)
+			frame.incHeal:SetValue(1)
+			frame.incHeal:SetStatusBarColor(0, 1, 0, 0.6)
+		end
 	end
-	local healed = HealComm:getHeal(UnitName(unit))
+end
+
+local function UnitFrameHealthBar_OnValueChangedHook(self)
+	HealComm:UpdateFrame(self, self.unit, currentHeals[UnitGUID(self.unit)] or 0)
+end
+
+local function UnitFrameHealthBar_OnUpdateHook(self)
+	if self.unit ~= "player" then return end
+	HealComm:UpdateFrame(self, self.unit, currentHeals[UnitGUID(self.unit)] or 0)
+end
+hooksecurefunc("UnitFrameHealthBar_OnUpdate", UnitFrameHealthBar_OnUpdateHook) -- This needs early hooking
+
+local function CompactUnitFrame_UpdateHealthHook(self)
+	HealComm:UpdateFrame(self.healthBar, self.displayedUnit, currentHeals[UnitGUID(self.displayedUnit)] or 0)
+end
+
+local function CompactUnitFrame_UpdateMaxHealthHook(self)
+	HealComm:UpdateFrame(self.healthBar, self.displayedUnit, currentHeals[UnitGUID(self.displayedUnit)] or 0)
+end
+
+local function CompactUnitFrame_SetUnitHook(self, unit)
+	if not self.healthBar.incHeal then
+		self.healthBar.incHeal = CreateFrame("StatusBar", self:GetName().."HealthBarIncHeal" , self)
+		self.healthBar.incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+		self.healthBar.incHeal:SetMinMaxValues(0, 1)
+		self.healthBar.incHeal:SetValue(1)
+		self.healthBar.incHeal:SetStatusBarColor(0, 1, 0, 0.6)
+	end
+end
+hooksecurefunc("CompactUnitFrame_SetUnit", CompactUnitFrame_SetUnitHook) -- This needs early hooking
+
+function HealComm:OnInitialize()
+	self:CreateBars()
+	hooksecurefunc("RaidPullout_Update", RaidPullout_UpdateHook)
+	hooksecurefunc("UnitFrameHealthBar_OnValueChanged", UnitFrameHealthBar_OnValueChangedHook)
+	hooksecurefunc("CompactUnitFrame_UpdateHealth", CompactUnitFrame_UpdateHealthHook)
+	hooksecurefunc("CompactUnitFrame_UpdateMaxHealth", CompactUnitFrame_UpdateMaxHealthHook)
+	libCHC.RegisterCallback(HealComm, "HealComm_HealStarted", "HealComm_HealUpdated")
+	libCHC.RegisterCallback(HealComm, "HealComm_HealStopped")
+	libCHC.RegisterCallback(HealComm, "HealComm_HealDelayed", "HealComm_HealUpdated")
+	libCHC.RegisterCallback(HealComm, "HealComm_HealUpdated")
+	libCHC.RegisterCallback(HealComm, "HealComm_ModifierChanged")
+	libCHC.RegisterCallback(HealComm, "HealComm_GUIDDisappeared")
+end
+
+function HealComm:CreateBars()
+	for unit,v in pairs(frames) do
+		v.bar.incHeal = CreateFrame("StatusBar", "IncHealBar"..unit, v.frame)
+		v.bar.incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+		v.bar.incHeal:SetMinMaxValues(0, 1)
+		v.bar.incHeal:SetValue(1)
+		v.bar.incHeal:SetStatusBarColor(0, 1, 0, 0.6)
+	end
+end
+
+function HealComm:UNIT_PET(unit)
+	if unit ~= "player" then return end
+	for guid,unit in pairs(partyGUIDs) do
+		if unit == "pet" then
+			partyGUIDs[guid] = nil
+			break
+		end
+	end
+	if UnitExists("pet") then
+		partyGUIDs[UnitGUID("pet")] = "pet"
+	end
+	self:UpdateFrame(frames["pet"].bar, "pet", currentHeals[UnitGUID("pet")] or 0)
+end
+
+function HealComm:PLAYER_TARGET_CHANGED()
+	self:UpdateFrame(frames["target"].bar, "target", currentHeals[UnitGUID("target")] or 0)
+end
+
+function HealComm:GROUP_ROSTER_UPDATE()
+	for guid,unit in pairs(partyGUIDs) do
+		if strsub(unit,1,5) == "party" then
+			partyGUIDs[guid] = nil
+		end
+	end
+	table.wipe(raidGUIDs)
+	
+	if UnitInParty("player") then
+		for i=1, MAX_PARTY_MEMBERS do
+			local p = "party"..i
+			if UnitExists(p) then
+				partyGUIDs[UnitGUID(p)] = p
+			else
+				break
+			end
+		end
+	end
+	if UnitInRaid("player") then
+		for i=1, MAX_RAID_MEMBERS do
+			local r = "party"..i
+			if UnitExists(r) then
+				raidGUIDs[UnitGUID(r)] = r
+			else
+				break
+			end
+		end
+	end
+end
+
+function HealComm:HealComm_HealUpdated(event, casterGUID, spellID, healType, endTime, ...)
+	self:UpdateIncoming(...)
+end
+
+function HealComm:HealComm_HealStopped(event, casterGUID, spellID, healType, interrupted, ...)
+	self:UpdateIncoming(...)
+end
+
+function HealComm:HealComm_ModifierChanged(event, guid)
+	self:UpdateIncoming(guid)
+end
+
+function HealComm:HealComm_GUIDDisappeared(event, guid)
+	self:UpdateIncoming(guid)
+end
+
+-- Handle callbacks from lib
+function HealComm:UpdateIncoming(...)
+	local amount, targetGUID
+	for i=1, select("#", ...) do
+		targetGUID = select(i, ...)
+		amount = (libCHC:GetHealAmount(targetGUID, libCHC.ALL_HEALS) or 0) * (libCHC:GetHealModifier(targetGUID) or 1)
+		currentHeals[targetGUID] = amount > 0 and amount
+		if UnitGUID("target") == targetGUID then
+			self:UpdateFrame(frames["target"].bar, "target", amount)
+		end
+		if partyGUIDs[targetGUID] then
+			self:UpdateFrame(frames[partyGUIDs[targetGUID]].bar, partyGUIDs[targetGUID], amount)
+		end
+		if UnitInRaid("player") then
+			local frame, unitframe
+			for k=1, NUM_RAID_PULLOUT_FRAMES do
+				frame = getglobal("RaidPullout"..k)
+				for z=1, frame.numPulloutButtons do
+					unitframe = getglobal(frame:GetName().."Button"..z)
+					if unitframe.unit and UnitGUID(unitframe.unit) == targetGUID then
+						self:UpdateFrame(getglobal(unitframe:GetName().."HealthBar"), unitframe.unit, amount)
+					end
+				end
+			end
+			--CompactRaidFrame<id>
+			unitframe = _G["CompactRaidFrame1"]
+			local num = 1
+			while unitframe do
+				if UnitGUID(unitframe.displayedUnit) == targetGUID then
+					self:UpdateFrame(unitframe.healthBar, unitframe.displayedUnit, amount)
+				end
+				num = num + 1
+				unitframe = _G["CompactRaidFrame"..num]
+			end
+		end
+	end
+end
+
+function HealComm:UpdateFrame(frame, unit, amount)
 	local health, maxHealth = UnitHealth(unit), UnitHealthMax(unit)
-	if( healed > 0 and (health < maxHealth or OVERHEALPERCENT > 0 )) and frame:IsVisible() then
+	if( amount > 0 and (health < maxHealth or OVERHEALPERCENT > 0 )) and frame:IsVisible() then
 		frame.incHeal:Show()
 		local healthWidth = frame:GetWidth() * (health / maxHealth)
-		local incWidth = frame:GetWidth() * (healed / maxHealth)
+		local incWidth = frame:GetWidth() * (amount / maxHealth)
 		if (healthWidth + incWidth) > (frame:GetWidth() * (1+(OVERHEALPERCENT/100)) ) then
 			incWidth = frame:GetWidth() * (1+(OVERHEALPERCENT/100)) - healthWidth
 		end
@@ -38,104 +206,18 @@ local function onHeal(unit, frameID)
 	else
 		frame.incHeal:Hide()
 	end
-end	
+end
 
-local function onEvent(unitname)
-	if UnitName("target") == unitname then
-		onHeal("target")
-	end
-	local unitobj = roster:GetUnitObjectFromName(unitname)
-	if not unitobj or not unitobj.unitid then
-		return
-	end
-	if UnitIsUnit("player", unitobj.unitid) then
-		onHeal("player")
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("PLAYER_TARGET_CHANGED")
+frame:RegisterEvent("GROUP_ROSTER_UPDATE")
+frame:RegisterEvent("UNIT_PET")
+frame:SetScript("OnEvent", function(self, event, ...)
+	if( event == "PLAYER_LOGIN" ) then
+		HealComm:OnInitialize()
+		self:UnregisterEvent("PLAYER_LOGIN")
 	else
-		for i=1, 4 do
-			if UnitIsUnit("party"..i, unitobj.unitid) then
-				onHeal("party"..i)
-			end
-		end
+		HealComm[event](HealComm, ...)
 	end
-	if UnitInRaid("player") then
-		local header = GROUP.." "..unitobj.subgroup
-		local classheader = UnitClass(unitobj.unitid)
-		local frame, unitframe
-		for i=1, NUM_RAID_PULLOUT_FRAMES do
-			frame = getglobal("RaidPullout"..i.."Name")
-			if frame:GetText() == header or frame:GetText() == classheader then
-				frame = getglobal("RaidPullout"..i)
-				for z=1, frame.numPulloutButtons do
-					unitframe = getglobal(frame:GetName().."Button"..z)
-					if unitframe.unit == unitobj.unitid then
-						onHeal(unitobj.unitid, getglobal(unitframe:GetName().."HealthBar"))
-					end
-				end
-			end
-		end
-	end
-end
-
-local function TargetChanged()
-	onHeal("target")
-end
-
-AceEvent:RegisterEvent("HealComm_Healupdate", onEvent)
-AceEvent:RegisterEvent("PLAYER_TARGET_CHANGED", TargetChanged)
-
-old_RaidPullout_Update = RaidPullout_Update
-function new_RaidPullout_Update(pullOutFrame)
-	old_RaidPullout_Update(pullOutFrame)
-	if ( not pullOutFrame ) then
-		pullOutFrame = this
-	end
-	local frame
-	for i=1, pullOutFrame.numPulloutButtons do
-		frame = getglobal(pullOutFrame:GetName().."Button"..i.."HealthBar")
-		if not frame.incheal then
-			frame.incHeal = CreateFrame("StatusBar", "PlayerFrameIncHealBar", frame)
-			frame.incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-			frame.incHeal:SetMinMaxValues(0, 1)
-			frame.incHeal:SetValue(1)
-			frame.incHeal:SetStatusBarColor(0, 1, 0, 0.6)
-		end
-	end
-end
-RaidPullout_Update = new_RaidPullout_Update
-
-
-frames["player"].incHeal = CreateFrame("StatusBar", "PlayerFrameIncHealBar", PlayerFrame)
-frames["player"].incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-frames["player"].incHeal:SetMinMaxValues(0, 1)
-frames["player"].incHeal:SetValue(1)
-frames["player"].incHeal:SetStatusBarColor(0, 1, 0, 0.5)
-
-frames["target"].incHeal = CreateFrame("StatusBar", "TargetFrameIncHealBar", TargetFrame)
-frames["target"].incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-frames["target"].incHeal:SetMinMaxValues(0, 1)
-frames["target"].incHeal:SetValue(1)
-frames["target"].incHeal:SetStatusBarColor(0, 1, 0, 0.5)
-
-frames["party1"].incHeal = CreateFrame("StatusBar", "PartyMemberFrame1IncHealBar", PartyMemberFrame1)
-frames["party1"].incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-frames["party1"].incHeal:SetMinMaxValues(0, 1)
-frames["party1"].incHeal:SetValue(1)
-frames["party1"].incHeal:SetStatusBarColor(0, 1, 0, 0.5)
-
-frames["party2"].incHeal = CreateFrame("StatusBar", "PartyMemberFrame2IncHealBar", PartyMemberFrame2)
-frames["party2"].incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-frames["party2"].incHeal:SetMinMaxValues(0, 1)
-frames["party2"].incHeal:SetValue(1)
-frames["party2"].incHeal:SetStatusBarColor(0, 1, 0, 0.5)
-
-frames["party3"].incHeal = CreateFrame("StatusBar", "PartyMemberFrame3IncHealBar", PartyMemberFrame3)
-frames["party3"].incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-frames["party3"].incHeal:SetMinMaxValues(0, 1)
-frames["party3"].incHeal:SetValue(1)
-frames["party3"].incHeal:SetStatusBarColor(0, 1, 0, 0.5)
-
-frames["party4"].incHeal = CreateFrame("StatusBar", "PartyMemberFrame4IncHealBar", PartyMemberFrame4)
-frames["party4"].incHeal:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-frames["party4"].incHeal:SetMinMaxValues(0, 1)
-frames["party4"].incHeal:SetValue(1)
-frames["party4"].incHeal:SetStatusBarColor(0, 1, 0, 0.5)
+end)
