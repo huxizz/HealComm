4a50a32c4d9ac46017159a216c66257e5141889a
Aviana <winblocker@gmx.de>
2019-06-08 21:28:10 +0200

    Fix Hots

--- HealComm.lua
+++ HealComm.lua
@@ -3,7 +3,7 @@ local libCHC = LibStub("LibClassicHealComm-1.0", true)
 OVERHEALPERCENT = 20
 
 HealComm = select(2, ...)
-HealComm.version = 2000
+HealComm.version = 2001
 
 local frames = {
 				["player"] = { bar = getglobal("PlayerFrameHealthBar"), frame = _G["PlayerFrame"] },
